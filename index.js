7db40af14c75a17311d943ccc61fcf0ee891d402
Robert Lützner <ro@iternity.com>
2020-03-21 11:55:45 +0100

    Return Zipcode Parameter from Search Query
    This is simply to get used to node.js and the Express library.

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function searchItems(req, res) {
+  location = req.query.zipcode;
+  response = {
+    zipcode:location
+  }
+  res.end(JSON.stringify(response))
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/api/search', searchItems)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
